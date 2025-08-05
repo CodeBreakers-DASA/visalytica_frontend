ed47efc49ed3bf86f27e0676614dd7eae41f5fb2
Alexia <alexia.rsantos2@gmail.com>
2025-08-05 00:07:02 -0300

    feat: implementação do layout de auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({
+  weight: ['400', '600'],
+  subsets: ['latin'],
+  display: 'swap'
+})
 
 export const metadata = {
   title: "Visalityca",
@@ -18,10 +14,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-br" className={inter.className}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+      >
         {children}
       </body>
     </html>
