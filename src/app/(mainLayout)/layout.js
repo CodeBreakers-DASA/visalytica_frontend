5c8b21c6fe6cb10b535b12fafa657743d216b5df
Alexia <alexia.rsantos2@gmail.com>
2025-09-27 18:02:55 -0300

    feat: integração login

--- src/app/(mainLayout)/layout.js
+++ src/app/(mainLayout)/layout.js
@@ -1,11 +1,14 @@
 import "../globals.css";
 import Hotbar from '../../components/Hotbar'
+import PrivateRoute from "../../components/PrivateRoute";
 
 export default function RootLayout({ children }) {
   return (
-    <main> 
+    <PrivateRoute> 
+      <main>
         <Hotbar />
-        {children} 
-    </main>
+          {children}
+      </main>
+    </PrivateRoute>
   );
 }
---
+++ src/components/Providers.js
@@ -0,0 +1,17 @@
+// components/Providers.js
+"use client";
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from '../context/AuthContext';
+
+const queryClient = new QueryClient();
+
+export default function Providers({ children }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        {children}
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
