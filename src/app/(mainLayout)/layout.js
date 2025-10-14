fd90988d4ce35fa4287bfa3d7e02e4aa4011a772
Lorenzo <lorenzzo.acquesta@gmail.com>
2025-10-14 18:59:36 -0300

    Feat:Tirando hotbar e colocando sidebar

--- src/app/(mainLayout)/layout.js
+++ src/app/(mainLayout)/layout.js
@@ -1,13 +1,13 @@
 import "../globals.css";
-import Hotbar from '../../components/Hotbar'
+import Hotbar from '../../components/Sidebar'
 import PrivateRoute from "../../components/PrivateRoute";
 
 export default function RootLayout({ children }) {
   return (
     <PrivateRoute> 
-      <div className="flex flex-col min-h-screen">
+      <div className="flex min-h-screen">
         <Hotbar />
-        <main className="flex-1 pt-[70px] xs:pt-[75px] sm:pt-[85px] md:pt-[95px] lg:pt-[101px] overflow-y-auto">
+        <main className="flex-1 pt-[70px] overflow-y-auto">
           <div className="px-3 xs:px-4 sm:px-6 md:px-8 lg:px-12 max-md:py-4 pt-4">
             {children}
           </div>
